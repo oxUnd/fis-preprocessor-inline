1eef48f4c82fc1b656ceb9d0d9916c3897ccd89d
xiangshouding <fansekey@gmail.com>
2013-09-02 12:25:04 +0800

    增加对其他变种css，js的支持，如less

--- index.js
+++ index.js
@@ -7,27 +7,44 @@
 
 var ld, rd;
 
-function hasInlineFile(file, rExt) {
-    var inlineFile = fis.file.wrap(file.realpathNoExt + '.inline' + rExt);
+function hasInlineFile(file, ext) {
+    var inlineFile = fis.file.wrap(file.realpathNoExt + '.inline.' + ext);
     if (inlineFile.exists()) {
         return inlineFile.subpath;
     }
     return false;
 }
 
-module.exports = function(content, file, conf) {
-
-    var ld = conf.left_delimiter || fis.config.get('settings.smarty.left_delimiter') || '{%';
-    var rd = conf.right_delimiter || fis.config.get('settings.smarty.right_delimiter') || '%}';
-    if (file.rExt == '.tpl') {
-        var inlineCss = hasInlineFile(file, '.css');
-        if (inlineCss) {
-            content = ld + 'style id="' + inlineCss +'"' + rd + fis.compile.extCss('@import url("' + inlineCss + '?__inline")') + ld + '/style' + rd + content;
-        }
-        var inlineJs = hasInlineFile(file, '.js');
-        if (inlineJs) {
-            content = ld + 'script id="' + inlineJs +'"' + rd + fis.compile.extJs('__inline("' + inlineJs + '")') + ld + '/script' + rd + content;
-        }
+function doInline(content, file, ext, type) {
+    var inline = hasInlineFile(file, ext);
+    if (!inline) {
+        return content;
     }
+    if (type === 'css') {
+        content = ld + 'style id="' + inline +'"' + rd + fis.compile.extCss('@import url("' + inline + '?__inline")') + ld + '/style' + rd + content;
+    } else if (type === 'js') {
+        content = ld + 'script id="' + inline +'"' + rd + fis.compile.extJs('__inline("' + inline + '")') + ld + '/script' + rd + content;
+    }
+    return content;
+}
+
+module.exports = function(content, file, conf) {
+    if (file.rExt !== '.tpl') {
+        return content;
+    }
+
+    ld = conf.left_delimiter || fis.config.get('settings.smarty.left_delimiter') || '{%';
+    rd = conf.right_delimiter || fis.config.get('settings.smarty.right_delimiter') || '%}';
+    var exts = fis.config.get('roadmap.ext') || {};
+
+    exts = fis.util.merge(exts, {
+        css: 'css',
+        js: 'js'
+    });
+
+    fis.util.map(exts, function (ext, type) {
+        content = doInline(content, file, ext, type);
+    });
+    
     return content;
 };
